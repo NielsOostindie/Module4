42e0dd29ebc371930afb432386c4ee2c82a16955
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2022-12-15 08:53:49 +0100

    push

---
+++ Workshop 1/assets/script/script.js
@@ -0,0 +1,51 @@
+var voornaam = "Jan"; // wijzig dit in jouw eigen voornaam
+var tussenvoegsel = "van"; // wijzig dit in jouw eigen eventuele tussenvoegsel
+var achternaam = "Os"; // wijzig dit in jouw eigen achternaam
+var volledigeNaam = "Jan van Os"; // plak hier de 3 bovenstaande variabelen aan elkaar, met spaties
+var geboortedag = "17"; // je geboortedag
+var geboortemaand = "01"; // je geboortemaand
+var geboortejaar = "2006"; // je geboortejaar
+var huidig_jaartal = "2022"// het huidige jaar;
+var leeftijd = 16; // bereken hier jouw leeftijd op basis van de eerder ingevulde geboortegegevens.
+var dag_van_het_jaar = 340; // bereken hier hoeveelste dag van dit kalenderjaar het is
+var straat = "zwanenwater"; // je straat;
+var huisnummer = "11"; // je huisnummer;
+var woonplaats = "lelystad"; // je woonplaats;
+var woonachtig_in_utrecht = false; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+var aantal_studenten_in_deze_klas = 22;
+var kosten_van_frikandelbroodje = 0.80;
+var totale_lunchkosten = 35.2; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+
+document.write(voornaam);
+document.write("<br>");
+document.write(tussenvoegsel);
+document.write('<br>');
+document.write(achternaam);
+document.write('<br>');
+document.write(volledigeNaam);
+document.write('<br>');
+document.write(geboortedag);
+document.write("<br>");
+document.write(geboortemaand);
+document.write("<br>");
+document.write(geboortejaar);
+document.write("<br>");
+document.write(huidig_jaartal);
+document.write("<br>");
+document.write(leeftijd);
+document.write("<br>");
+document.write(dag_van_het_jaar);
+document.write("<br>");
+document.write(straat);
+document.write("<br>");
+document.write(huisnummer);
+document.write("<br>");
+document.write(woonplaats);
+document.write("<br>");
+document.write(woonachtig_in_utrecht);
+document.write("<br>");
+document.write(aantal_studenten_in_deze_klas);
+document.write('<br>');
+document.write(kosten_van_frikandelbroodje);
+document.write('<br>');
+document.write(totale_lunchkosten);
---
+++ Workshop 2/assets/js/script.js
@@ -0,0 +1,40 @@
+document.getElementById("i").onclick = function(){ // als op de i-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het i-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("j").onclick = function(){// als op de j-toets wordt geklikt
+	var d_toets = document.getElementById("audio_d");// zoek het j-toets audio-element op
+	d_toets.play();
+}
+document.getElementById("k").onclick = function(){// als op de k-toets wordt geklikt
+	var e_toets = document.getElementById("audio_e");// zoek het k-toets audio-element op
+	e_toets.play();// speel het audio-element af
+}
+document.getElementById("a").onclick = function(){// als op de a-toets wordt geklikt
+	var a_toets = document.getElementById("audio_a");// zoek het a-toets audio-element op
+	a_toets.play();// speel het audio-element af
+}
+document.getElementById("b").onclick = function(){// als op de b-toets wordt geklikt
+	var b_toets = document.getElementById("audio_b");// zoek het b-toets audio-element op
+	b_toets.play();// speel het audio-element af
+}
+document.getElementById("c").onclick = function(){// als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c");// zoek het c-toets audio-element op
+	c_toets.play();// speel het audio-element af
+}
+document.getElementById("d").onclick = function(){// als op de d-toets wordt geklikt
+	var d_toets = document.getElementById("audio_d");// zoek het d-toets audio-element op
+	d_toets.play();// speel het audio-element af
+}
+document.getElementById("e").onclick = function(){// als op de e-toets wordt geklikt
+	var e_toets = document.getElementById("audio_e");// zoek het e-toets audio-element op
+	e_toets.play();// speel het audio-element af
+}
+document.getElementById("f").onclick = function(){// als op de f-toets wordt geklikt
+	var f_toets = document.getElementById("audio_f");// zoek het f-toets audio-element op
+	f_toets.play();// speel het audio-element af
+}
+document.getElementById("g").onclick = function(){// als op de g-toets wordt geklikt
+	var g_toets = document.getElementById("audio_g");// zoek het g-toets audio-element op
+	g_toets.play();// speel het audio-element af
+}
