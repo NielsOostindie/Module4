6a565dad0e2bb14abe39545a4709a533e8bd5c66
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2022-12-16 11:03:21 +0100

    workshop 3 af

--- Workshop 3/assets/script/script.js
+++ Workshop 3/assets/script/script.js
@@ -1,120 +1,68 @@
-"use strict";
-
-var input = document.getElementById('input'), // input/output button
-  number = document.querySelectorAll('.numbers div'), // number buttons
-  operator = document.querySelectorAll('.operators div'), // operator buttons
-  result = document.getElementById('result'), // equal button
-  clear = document.getElementById('clear'), // clear button
-  resultDisplayed = false; // flag to keep an eye on what output is displayed
-
-// adding click handlers to number buttons
-for (var i = 0; i < number.length; i++) {
-  number[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
-
-    // if result is not diplayed, just keep adding
-    if (resultDisplayed === false) {
-      input.innerHTML += e.target.innerHTML;
-    } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-      // if result is currently displayed and user pressed an operator
-      // we need to keep on adding to the string for next operation
-      resultDisplayed = false;
-      input.innerHTML += e.target.innerHTML;
-    } else {
-      // if result is currently displayed and user pressed a number
-      // we need clear the input string and add the new input to start the new opration
-      resultDisplayed = false;
-      input.innerHTML = "";
-      input.innerHTML += e.target.innerHTML;
-    }
-
-  });
+function knop9() {
+  document.getElementById("output").innerHTML += 9;
+}
+function knop8() {
+  document.getElementById("output").innerHTML += 8;
+}
+function knop7() {
+  document.getElementById("output").innerHTML += 7;
 }
 
-// adding click handlers to number buttons
-for (var i = 0; i < operator.length; i++) {
-  operator[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
-
-    // if last character entered is an operator, replace it with the currently pressed one
-    if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-      var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-      input.innerHTML = newString;
-    } else if (currentString.length == 0) {
-      // if first key pressed is an opearator, don't do anything
-      console.log("enter a number first");
-    } else {
-      // else just add the operator pressed to the input
-      input.innerHTML += e.target.innerHTML;
-    }
-
-  });
+function knop6() {
+  document.getElementById("output").innerHTML += 6;
 }
 
-// on click of 'equal' button
-result.addEventListener("click", function() {
+function knop5() {
+  document.getElementById("output").innerHTML += 5;
+}
+function knop4() {
+  document.getElementById("output").innerHTML += 4;
+}
 
-  // this is the string that we will be processing eg. -10+26+33-56*34/23
-  var inputString = input.innerHTML;
+function knop3() {
+  document.getElementById("output").innerHTML += 3;
+}
+function knop2() {
+  document.getElementById("output").innerHTML += 2;
+}
+function knop1() {
+  document.getElementById("output").innerHTML += 1;
+}
 
-  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-  var numbers = inputString.split(/\+|\-|\×|\÷/g);
+function knop0() {
+  document.getElementById("output").innerHTML += 0;
+}
 
-  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-  // first we replace all the numbers and dot with empty string and then split
-  var operators = inputString.replace(/[0-9]|\./g, "").split("");
+function knopClear() {
+  document.getElementById("output").innerHTML = "";
+}
 
-  console.log(inputString);
-  console.log(operators);
-  console.log(numbers);
-  console.log("----------------------------");
+function knopKeer() {
+  document.getElementById("output").innerHTML += "x";
+}
+function knopPlus() {
+  document.getElementById("output").innerHTML += "+";
+}
 
-  // now we are looping through the array and doing one operation at a time.
-  // first divide, then multiply, then subtraction and then addition
-  // as we move we are alterning the original numbers and operators array
-  // the final element remaining in the array will be the output
+function knopMin() {
+  document.getElementById("output").innerHTML += "-";
+}
+function knopGedeelddoor() {
+  document.getElementById("output").innerHTML += "/";
+}
 
-  var divide = operators.indexOf("÷");
-  while (divide != -1) {
-    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-    operators.splice(divide, 1);
-    divide = operators.indexOf("÷");
-  }
+function knopIs() {
+  let x = 10;
+  let y = 20;
+  let string = document.getElementById("output").innerHTML;
+  string = string.replace("x", "*");
+  string = string.replace("÷", "/");
+  string = string.replace("plus", "+");
+  let result = eval(string);
+  document.getElementById("output").innerHTML = result;
+}
 
-  var multiply = operators.indexOf("×");
-  while (multiply != -1) {
-    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-    operators.splice(multiply, 1);
-    multiply = operators.indexOf("×");
-  }
-
-  var subtract = operators.indexOf("-");
-  while (subtract != -1) {
-    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-    operators.splice(subtract, 1);
-    subtract = operators.indexOf("-");
-  }
-
-  var add = operators.indexOf("+");
-  while (add != -1) {
-    // using parseFloat is necessary, otherwise it will result in string concatenation :)
-    numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-    operators.splice(add, 1);
-    add = operators.indexOf("+");
-  }
-
-  input.innerHTML = numbers[0]; // displaying the output
-
-  resultDisplayed = true; // turning flag if result is displayed
-});
-
-// clearing the input on press of clear
-clear.addEventListener("click", function() {
-  input.innerHTML = "";
-})
+let string = "8x8";
+string = string.replace("x", "*");
+string = string.replace("÷", "/");
+string = string.replace("plus", "+");
